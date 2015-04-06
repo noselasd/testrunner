ca25ae5bfbd0cf15ca03d0714dafd0a40431210e
Nils O. Selåsdal <noselasd@fiane.dyndns.org>
2015-04-06 14:24:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8
+
+from distutils.core import setup, Extension
+
+setup(name='testrunner',
+      version="0.0.1",
+      description='A simple testing harness',
+      author='Nils O. Selåsdal',
+      author_email='noselasd@fiane.mooo.com',
+      scripts=['testrunner.py']
+     )
